89921c8973229795aa373faf90a050d4f1ed0228
sasahardi997 <sasahardi@gmail.com>
2021-11-12 21:41:35 +0100

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './auth';
+import counterSlice from './counter';
+
+const store = configureStore({
+    reducer: {
+        counter: counterSlice,
+        auth: authSlice
+    }
+});
+
+export default store;
